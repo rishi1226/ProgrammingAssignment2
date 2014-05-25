0b297166d631e97f805ffc16985247d6bebacd1e
rishi1226 <agnihotri.rishabh@gmail.com>
2014-05-25 16:23:59 +0530

    Committing Prog. Ass. 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Excercise to understand implement caching values of potentially expensive
+## operations. e.g. Computing inverse of a matrix
 
-## Write a short comment describing this function
+## Defines a special "MATRIX" object that stores routines to set and retrieve 
+##it along with that of its its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        
+        # defines 4 fuctions to get and set matrix and its inverse
+        set <- function(m) {
+                x <<- m
+                inverse <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(inv) inverse <<- inv
+        getinverse <- function() inverse
+        
+        #defines a special matrix object
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of square matrix 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #retrieves inverse of matrix. If NULL then it is calulated. If not it is retrieved from cache
+        inv <- x$getinverse()
+        if (!is.null(inv)) {
+                message("Getting cached result")
+                return(inv)
+        } else {
+                inv <- solve(x$get())
+                x$setinverse(inv)
+                return(inv)
+        }
 }
